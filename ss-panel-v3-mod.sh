399b014063d7e3a9c1783b10387dba0e399707c9
mmmwhy <mmmwhy@qq.com>
2017-11-26 13:15:16 +0800

    no message

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -102,11 +102,11 @@ install_centos_ssr(){
 	cd /root
 	Get_Dist_Version
 	if [ $Version == "7" ]; then
-		wget http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm   
-		rpm -ivh epel-release-7-10.noarch.rpm	
+		wget --no-check-certificate https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
+		rpm -ivh epel-release-latest-7.noarch.rpm	
 	else
-		wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-		rpm -ivh epel-release-6-8.noarch.rpm
+		wget --no-check-certificate https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+		rpm -ivh epel-release-latest-6.noarch.rpm
 	fi
 	rm -rf *.rpm
 	yum -y update --exclude=kernel*	
